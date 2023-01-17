cb3a3e34fda0d8cb0874bcc7fc55d773ca68f276
ítalo Fabrício <italofps65@gmail.com>
2023-01-17 12:28:20 -0300

    feat: criando o loading de carregamento

---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .loading{
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        border: 3px solid @light-yellow;
+        border-top: @yellow;
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin{
+        to{
+            transform: rotate(360deg);
+        }
+    }
+</style>
